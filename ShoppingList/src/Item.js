c7af82253ce4c3d2474425c401564743e4341a28
Natalia Vera Duran <naveduran@gmail.com>
2022-01-10 15:12:03 -0500

    [Add] prop types

--- ShoppingList/src/Item.js
+++ ShoppingList/src/Item.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Item extends Component {
   render() {
     const {item, index} = this.props
     return ( <li key={index}>{item}</li> )
   }
+}
+
+Item.propTypes = {
+	item: PropTypes.string.isRequired,
+  	index: PropTypes.string.isRequired,
 }
 
 export default Item;
